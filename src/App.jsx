af785b4edee8b01442ec74576aa01ac18b894650
tordvb10 <tordvb10.kodehode@gmail.com>
2024-03-08 09:41:17 +0100

    ferdig med component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
 import React from 'react'
-import { ReactDOM } from 'react'
+//import { ReactDOM } from 'react'
 import './App.css'
-import content from "../verses"
+import content from "../verses.json"
 //import scripturesFetch from './scripturesFetch.js'
-console.log(content)
-
-function App() {
+function ComponentBibleVerse() {
   const verse = content.verseId[Math.floor(Math.random()*content.verseId.length)]
-  const listItems = (
-    <li key={verse}>{verse}
-    </li>
-  )
-  console.log(verse)
-  console.log(listItems)
-
+  const listItems =  <li key={verse}>{verse}</li>
   return (
     <React.Fragment>  
       <ul className='lister'>{listItems}</ul>
     </React.Fragment>
   )
+}
+function App() {
+  return (
+    <React.Fragment>
+      <ComponentBibleVerse />
+      <ComponentBibleVerse />
+      <ComponentBibleVerse />
+      <ComponentBibleVerse />
+    </React.Fragment>
+  )  
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <App />
-    <App />
-    <App />
-     
+    <App />     
   </React.StrictMode>,
 )
